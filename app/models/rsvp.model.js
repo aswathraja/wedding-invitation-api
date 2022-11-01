f7cd1bb1434ea416b02a005e4d22acbc31ab0754
Ash <aswathraja@gmail.com>
2022-11-01 08:54:14 +0530

    Update rsvp.model.js

--- app/models/rsvp.model.js
+++ app/models/rsvp.model.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, Sequelize) => {
 			type: Sequelize.STRING,
 			allowNull: true,
 		},
-		guestType: {
+		guesttype: {
 			type: Sequelize.STRING,
 			allowNull: false,
 		},
